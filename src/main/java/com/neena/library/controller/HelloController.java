34069ac4ff854454538b25a38bcb584b7f1237e0
Neena Damodaran <neenadamodaran@Neenas-MBP-2.fritz.box>
2019-07-28 00:07:21 +0200

    commiting the login screens

--- src/main/java/com/neena/library/controller/HelloController.java
+++ src/main/java/com/neena/library/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping(value ={ "/", "/hello"}, method=RequestMethod.GET)
+    @RequestMapping(value ={"/hello"}, method=RequestMethod.GET)
     public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
